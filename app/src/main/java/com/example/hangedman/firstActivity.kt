4b6d63246874d14100fd79803cbc479b611a7360
DiSSNiKO <nikokalanda@gmail.com>
2021-12-29 20:58:50 +0400

    bruh

--- app/src/main/java/com/example/hangedman/firstActivity.kt
+++ app/src/main/java/com/example/hangedman/firstActivity.kt
@@ -2,10 +2,35 @@ package com.example.hangedman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.content.Intent
+import android.widget.Toast
 
 class firstActivity : AppCompatActivity() {
+    lateinit var secretwordET : EditText
+    lateinit var startButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.firstActivity)
+        setContentView(R.layout.first_activity)
+        var numOfChars = 0
+        secretwordET = findViewById(R.id.chosenword)
+        startButton = findViewById(R.id.startGame)
+        startButton.setOnClickListener{
+            var secretword = secretwordET.text.toString()
+            for(x in secretwordET.text.toString()){numOfChars+=1}
+            if(secretwordET.text.isEmpty()||numOfChars>30){
+                numOfChars = 0
+                secretword = ""
+                Toast.makeText(this, "Should not be empty or more than 30 characters >:(", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            var intent = Intent(this, gameActivity::class.java)
+            intent.putExtra("secretWord", secretword)
+            startActivity(intent)
+        }
     }
 }
+
+
