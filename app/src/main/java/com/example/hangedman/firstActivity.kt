232d3e697b268ae19ff2c538def23e7f05eebc16
DiSSNiKO <nikokalanda@gmail.com>
2021-12-29 19:34:36 +0400

    Initial commit

---
+++ app/src/main/java/com/example/hangedman/firstActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hangedman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class firstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.firstActivity)
+    }
+}
